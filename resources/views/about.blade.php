f5b56ac6933961718777460a1dd62be7f80069d3
rizani10 <faisalrizani12@gmail.com>
2022-02-10 09:36:06 +0800

    Commit keduaa

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <h1>Halaman About</h1>
-    <h3><?= $name ?></h3>
-    <h3><?= $email; ?></h3>
-    <img src="img/<?= $img; ?>" alt="<?= $name; ?>" width="100">
+    <h3> {{ $name }} </h3>
+    <h3>{{ $email }}</h3>
+    <img src="img/{{ $img }}" alt="{{ $name }}" width="100">
 </body>
 </html>
